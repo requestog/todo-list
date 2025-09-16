ff4469db0204949d4f9d262355e837f1a24900e7
Sergey <119594843+gordondev@users.noreply.github.com>
2025-09-17 00:30:21 +0500

    change app.module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,16 @@
-import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppService} from './app.service';
+import {ConfigModule} from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: `.${process.env.NODE_ENV}.env`,
+        }),
+    ],
+    controllers: [],
+    providers: [AppService],
 })
+
 export class AppModule {}
