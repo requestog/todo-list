2d6503f30711522bb0cee22cfa92582095fd3067
Sergey <119594843+gordondev@users.noreply.github.com>
2025-09-17 14:55:33 +0500

    add message to dtos

--- server/src/modules/auth/dto/logout.dto.ts
+++ server/src/modules/auth/dto/logout.dto.ts
@@ -1,7 +1,7 @@
-import {IsNotEmpty, IsString} from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class LogoutDto {
-    @IsString()
-    @IsNotEmpty()
+    @IsString({ message: 'idSession должен быть строкой' })
+    @IsNotEmpty({ message: 'idSession не должен быть пустым' })
     idSession: string;
 }
--- server/src/modules/todo/dto/todo.dto.ts
+++ server/src/modules/todo/dto/todo.dto.ts
@@ -1,14 +1,14 @@
 import { IsString, IsBoolean, IsNotEmpty, IsMongoId } from 'class-validator';
 
 export class TodoDto {
-    @IsNotEmpty()
-    @IsMongoId()
+    @IsNotEmpty({ message: 'Поле userId не должно быть пустым' })
+    @IsMongoId({ message: 'userId должен быть валидным MongoDB ObjectId' })
     userId: string;
 
-    @IsNotEmpty()
-    @IsString()
+    @IsNotEmpty({ message: 'Поле title не должно быть пустым' })
+    @IsString({ message: 'title должен быть строкой' })
     title: string;
 
-    @IsBoolean()
+    @IsBoolean({ message: 'completed должно быть булевым значением' })
     completed?: boolean;
 }
